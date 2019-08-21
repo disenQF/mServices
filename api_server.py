01b40d97840b46fd383c87a41524a9636175f8ee
disen-xaqf <610039018@qq.com>
2019-08-21 20:33:49 +0800

    优化跨域请求问题

--- api_server.py
+++ api_server.py
@@ -17,42 +17,37 @@ class LoginHandler(RequestHandler):
         'last_login_device': 'Android 5.1 OnePlus5'
     }]
 
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Headers', 'Content-Type,x-requested-with')
+        self.set_header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+
+
     def post(self):
         # 读取json数据
         bytes = self.request.body  # 字节类型
         print(bytes)
-        print(self.request.headers.get('Content-Type'))
+        json_str = bytes.decode('utf-8')
+        # 反序列化
+        json_data = json.loads(json_str)
 
-        # 从请求头中读取请求上传的数据类型 (body的数据类型)
-        content_type = self.request.headers.get('Content-Type')
-        if content_type.startswith('application/json'):
-            # self.write('upload json ok')
-            json_str = bytes.decode('utf-8')
-            # 反序列化
-            json_data = json.loads(json_str)
+        resp_data = {}
+        login_user = None
+        # 查询用户名和口令是否正确
+        for user in self.users:
+            if user['name'] == json_data['name']:
+                if user['pwd'] == json_data['pwd']:
+                    login_user = user
+                    break
 
-
-            resp_data = {}
-            login_user = None
-            # 查询用户名和口令是否正确
-            for user in self.users:
-                if user['name'] == json_data['name']:
-                    if user['pwd'] == json_data['pwd']:
-                        login_user = user
-                        break
-
-            if login_user:
-                resp_data['msg'] = 'success'
-                resp_data['token'] = uuid.uuid4().hex
-            else:
-                resp_data['msg'] = '查无此用户'
-
-            self.write(resp_data) # write()函数可接收str, dict, list
-            self.set_header('Content-Type', 'application/json')
+        if login_user:
+            resp_data['msg'] = 'success'
+            resp_data['token'] = uuid.uuid4().hex
         else:
-            self.write('uppload data 必须是json格式')
+            resp_data['msg'] = '查无此用户'
 
-        self.cors()
+        self.write(resp_data)  # write()函数可接收str, dict, list
+        self.set_header('Content-Type', 'application/json')
 
     def get(self):
         pass
@@ -60,13 +55,11 @@ class LoginHandler(RequestHandler):
     def put(self):
         pass
 
+    def options(self):
+        self.set_status(200)
+
     def delete(self):
         pass
-
-    def cors(self):
-        self.set_header('Access-Control-Allow-Origin', '*')
-        self.set_header('Access-Control-Allow-Headers', 'x-requested-with')
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
 
     def on_finish(self):
         pass
